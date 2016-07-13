db250928cdb4e54ba2490d2cebe5d5067f2da559
Stefan Huchler <stefan.huchler@mail.de>
2016-07-13 11:19:34 +0200

    fixes small last minute regression
    the definition of the which interpreter to use if you call
    the script directly was not in the first line where it have to be

--- youtube-populator.py
+++ youtube-populator.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 # Copyright (C) 2016 Stefan Huchler
 
 # This program is free software: you can redistribute it and/or modify
@@ -12,8 +13,6 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-#!/bin/python3
 
 
 import subprocess
